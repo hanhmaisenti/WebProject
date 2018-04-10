7f82aa3c58ebd3b016a8be7f9e89d3be7fa245dc
hanhmaisenti <36471317+hanhmaisenti@users.noreply.github.com>
2018-04-10 13:36:32 +0200

    Update admindb.php

--- admin/admindb.php
+++ admin/admindb.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL*/
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'letmein');
-define('DB_NAME', 'Interview');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'interview');
 
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die($mysqli>error);
