724327d2048c98915de96f6aa99f3da0c1943d5b
hanhmaisenti <36471317+hanhmaisenti@users.noreply.github.com>
2018-04-10 13:39:25 +0200

    Update candidatedb.php

--- candidatedb.php
+++ candidatedb.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL*/
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'letmein');
-define('DB_NAME', 'Interview'); //different database to the admin database
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'interview'); //different database to the admin database
 
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die($mysqli>error);
