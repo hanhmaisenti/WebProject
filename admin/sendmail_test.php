a01d3665293994a01f1dfcc511d1396a363bf097
hanhmaisenti <36471317+hanhmaisenti@users.noreply.github.com>
2018-04-10 13:41:22 +0200

    Update sendmail_test.php

--- admin/sendmail_test.php
+++ admin/sendmail_test.php
@@ -1,8 +1,8 @@
 <?php
-    $to      = "hanh.maisenti@outlook.com";
+    $to      = "tinytilly@gmail.com";
     $subject = "Password Reset Link ( Interview Portal )";
     $message_body = "blah blah";
-    $headers = "from: hanh.maisenti@outlook.com";
+    $headers = "from: tinytilly@gmail.com";
     if (mail($to, $subject, $message_body,$headers)) {
         echo "WORKED!";
     } else {
